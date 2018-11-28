607997add0a4e145aa733b49fcc020e299102ac3
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2018-11-28 00:49:29 -0200

    Add Visual Studio Solution

--- test/vec2_test.cpp
+++ test/vec2_test.cpp
@@ -1,15 +1,15 @@
-#include <gtest/gtest.h>
+#include "pch.h"
 #include <math/vec2.h>
 
 namespace {
 
-  class Vec2Test : public ::testing::Test{};
+	class Vec2Test : public ::testing::Test{};
 
-  TEST_F(Vec2Test, setValues)
-  {
-    math::vec2 position(1.5f, 2.0f);
-    EXPECT_EQ(position.x, 1.5f);
-    EXPECT_EQ(position.y, 2.0f);
-  }
+	TEST_F(Vec2Test, setValues)
+	{
+		math::vec2 position(1.5f, 2.0f);
+		EXPECT_EQ(position.x, 1.5f);
+		EXPECT_EQ(position.y, 2.0f);
+	}
 
 }
