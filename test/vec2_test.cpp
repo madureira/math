a3fe77fd435ee132cfd93c9d7abf38d682bea073
Rafael Madureira dos Santos <rafael.madureira.santos@gmail.com>
2018-11-29 22:45:53 -0200

    Refactor on vec2_test

--- test/vec2_test.cpp
+++ test/vec2_test.cpp
@@ -7,184 +7,152 @@ namespace {
 
 	TEST_F(Vec2Test, setDefaultValues)
 	{
-		math::vec2 position;
-		EXPECT_EQ(position.x, 0.0f);
-		EXPECT_EQ(position.y, 0.0f);
+		math::vec2 vec;
+
+		EXPECT_EQ(vec.x, 0.0f);
+		EXPECT_EQ(vec.y, 0.0f);
 	}
 
 	TEST_F(Vec2Test, setValuesByConstructor)
 	{
-		math::vec2 position(1.5f, 2.0f);
-		EXPECT_EQ(position.x, 1.5f);
-		EXPECT_EQ(position.y, 2.0f);
+		math::vec2 vec(1.5f, 2.0f);
+
+		EXPECT_EQ(vec.x, 1.5f);
+		EXPECT_EQ(vec.y, 2.0f);
 	}
 
 	TEST_F(Vec2Test, initiateANewVec2ByCopy)
 	{
-		math::vec2 position(10.0f, 20.0f);
+		math::vec2 vec(math::vec2(10.0f, 20.0f));
 
-		math::vec2 coordinates(position);
-
-		EXPECT_EQ(coordinates.x, 10.0f);
-		EXPECT_EQ(coordinates.y, 20.0f);
+		EXPECT_EQ(vec.x, 10.0f);
+		EXPECT_EQ(vec.y, 20.0f);
 	}
 
 	TEST_F(Vec2Test, setX)
 	{
-		math::vec2 position;
-		position.x = 30.0f;
-		EXPECT_EQ(position.x, 30.0f);
+		math::vec2 vec;
+		vec.x = 30.0f;
+
+		EXPECT_EQ(vec.x, 30.0f);
 	}
 
 	TEST_F(Vec2Test, setY)
 	{
-		math::vec2 position;
-		position.y = 20.0f;
-		EXPECT_EQ(position.y, 20.0f);
+		math::vec2 vec;
+		vec.y = 20.0f;
+
+		EXPECT_EQ(vec.y, 20.0f);
 	}
 
 	TEST_F(Vec2Test, addAVec2)
 	{
-		math::vec2 position(10.0f, 20.0f);
+		math::vec2 vec(10.0f, 20.0f);
+		vec.add(math::vec2(20.0f, 30.0f));
 
-		math::vec2 coordinates(20.0f, 30.0f);
-
-		position.add(coordinates);
-
-		EXPECT_EQ(position.x, 30.0f);
-		EXPECT_EQ(position.y, 50.0f);
+		EXPECT_EQ(vec.x, 30.0f);
+		EXPECT_EQ(vec.y, 50.0f);
 	}
 
 	TEST_F(Vec2Test, subtractAVec2)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec.subtract(math::vec2(5.0f, 25.0f));
 
-		math::vec2 coordinates(5.0f, 25.0f);
-
-		position.subtract(coordinates);
-
-		EXPECT_EQ(position.x, 15.0f);
-		EXPECT_EQ(position.y, 5.0f);
+		EXPECT_EQ(vec.x, 15.0f);
+		EXPECT_EQ(vec.y, 5.0f);
 	}
 
 	TEST_F(Vec2Test, multiplyAVec2)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec.multiply(math::vec2(2.0f, 3.0f));
 
-		math::vec2 coordinates(2.0f, 3.0f);
-
-		position.multiply(coordinates);
-
-		EXPECT_EQ(position.x, 40.0f);
-		EXPECT_EQ(position.y, 90.0f);
+		EXPECT_EQ(vec.x, 40.0f);
+		EXPECT_EQ(vec.y, 90.0f);
 	}
 
 	TEST_F(Vec2Test, divideAVec2)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
+		vec.divide(math::vec2(2.0f, 3.0f));
 
-		position.divide(coordinates);
-
-		EXPECT_EQ(position.x, 10.0f);
-		EXPECT_EQ(position.y, 10.0f);
+		EXPECT_EQ(vec.x, 10.0f);
+		EXPECT_EQ(vec.y, 10.0f);
 	}
 
 	TEST_F(Vec2Test, operatorAdd)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
+		vec = vec + math::vec2(2.0f, 3.0f);
 
-		position = position + coordinates;
-
-		EXPECT_EQ(position.x, 22.0f);
-		EXPECT_EQ(position.y, 33.0f);
+		EXPECT_EQ(vec.x, 22.0f);
+		EXPECT_EQ(vec.y, 33.0f);
 	}
 
 	TEST_F(Vec2Test, operatorSubtract)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec = vec - math::vec2(2.0f, 3.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
-
-		position = position - coordinates;
-
-		EXPECT_EQ(position.x, 18.0f);
-		EXPECT_EQ(position.y, 27.0f);
+		EXPECT_EQ(vec.x, 18.0f);
+		EXPECT_EQ(vec.y, 27.0f);
 	}
 
 	TEST_F(Vec2Test, operatorMultiply)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec = vec * math::vec2(2.0f, 3.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
-
-		position = position * coordinates;
-
-		EXPECT_EQ(position.x, 40.0f);
-		EXPECT_EQ(position.y, 90.0f);
+		EXPECT_EQ(vec.x, 40.0f);
+		EXPECT_EQ(vec.y, 90.0f);
 	}
 
 	TEST_F(Vec2Test, operatorDivide)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec = vec / math::vec2(2.0f, 3.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
-
-		position = position / coordinates;
-
-		EXPECT_EQ(position.x, 10.0f);
-		EXPECT_EQ(position.y, 10.0f);
+		EXPECT_EQ(vec.x, 10.0f);
+		EXPECT_EQ(vec.y, 10.0f);
 	}
 
 	TEST_F(Vec2Test, operatorPlusEquals)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec += math::vec2(2.0f, 3.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
-
-		position += coordinates;
-
-		EXPECT_EQ(position.x, 22.0f);
-		EXPECT_EQ(position.y, 33.0f);
+		EXPECT_EQ(vec.x, 22.0f);
+		EXPECT_EQ(vec.y, 33.0f);
 	}
 
 	TEST_F(Vec2Test, operatorMinusEquals)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec -= math::vec2(2.0f, 3.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
-
-		position -= coordinates;
-
-		EXPECT_EQ(position.x, 18.0f);
-		EXPECT_EQ(position.y, 27.0f);
+		EXPECT_EQ(vec.x, 18.0f);
+		EXPECT_EQ(vec.y, 27.0f);
 	}
 
 	TEST_F(Vec2Test, operatorTimesEquals)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec *= math::vec2(2.0f, 3.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
-
-		position *= coordinates;
-
-		EXPECT_EQ(position.x, 40.0f);
-		EXPECT_EQ(position.y, 90.0f);
+		EXPECT_EQ(vec.x, 40.0f);
+		EXPECT_EQ(vec.y, 90.0f);
 	}
 
 	TEST_F(Vec2Test, operatorDevideEquals)
 	{
-		math::vec2 position(20.0f, 30.0f);
+		math::vec2 vec(20.0f, 30.0f);
+		vec /= math::vec2(2.0f, 3.0f);
 
-		math::vec2 coordinates(2.0f, 3.0f);
-
-		position /= coordinates;
-
-		EXPECT_EQ(position.x, 10.0f);
-		EXPECT_EQ(position.y, 10.0f);
+		EXPECT_EQ(vec.x, 10.0f);
+		EXPECT_EQ(vec.y, 10.0f);
 	}
 
 }
